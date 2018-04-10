f21a2fa34d09b8ab039222b078d6dd6b26d42a9d
Haggai Lazaro <haggai@Haggais-MBP.hitronhub.home>
2018-04-10 06:41:08 -0600

    updated

--- web-services.js
+++ web-services.js
@@ -240,7 +240,7 @@ app.post('/login1', (req, resp) => {
 });
 
 // after email found, check password
-app.post('/api/login2', (req, resp) => {
+app.post('/login2', (req, resp) => {
     var pass = md5(req.body.pw + req.body.salt, 'hex');
     User.find({password: pass}, {id: 1, first_name: 1, last_name: 1, email: 1}, (err, data) => {
             if (err) {
