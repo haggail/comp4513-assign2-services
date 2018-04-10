ed24fbb85a0b3c7c17ee52be44f308c50c32e120
Haggai Lazaro <haggai@Haggais-MBP.hitronhub.home>
2018-04-10 05:13:13 -0600

    configuring login

--- web-services.js
+++ web-services.js
@@ -63,7 +63,6 @@ var User = mongoose.model('User', userSchema);
 var app = express();
 app.use(function (req, res, next) {
     //res.setHeader('Access-Control-Allow-Origin', 'https://wiggly-kitty.herokuapp.com');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
     next();
 });	
